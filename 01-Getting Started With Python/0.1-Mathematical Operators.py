e5e9ce1ebde629d31221f1a37ecaee96c730043b
aleynacetin1 <aleynacetin1.official.@gmail.com>
2023-08-11 23:08:13 +0300

    math operators intro

---
+++ 01-Getting Started With Python/0.1-Mathematical Operators.py
@@ -0,0 +1,29 @@
+#MATHEMATICAL OPERATORS!!
+
+# "+"  "-"  "*"  "/"  "%"  "**"  "//"
+a=47
+b=8
+
+print(a,'+',b,'=',a+b)
+print(a,'-',b,'=',a-b)
+print(a,'*',b,'=',a*b)
+print(a,'/',b,'=',a/b)
+print(a,'%',b,'=',a%b)
+print(a,'**',b,'=',a**b)
+print(a,'//',b,'=',a//b)
+
+c=76
+d=91
+toplam=c+d
+print('Toplam =',toplam)
+
+"""OUTPUT
+47 + 8 = 55
+47 - 8 = 39
+47 * 8 = 376
+47 / 8 = 5.875
+47 % 8 = 7
+47 ** 8 = 23811286661761
+47 // 8 = 5
+Toplam = 167
+"""
